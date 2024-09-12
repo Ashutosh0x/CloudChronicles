085e49904980f0d82edd978e87150c4dcec66709
Ashutosh Kumar Singh <161562995+Ashutosh0x@users.noreply.github.com>
2024-09-13 01:36:17 +0530

    route.ts

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -31,7 +31,7 @@ export async function GET() {
 
   return NextResponse.redirect(
     process.env.NODE_ENV === "production"
-      ? "https://cloudchronicles.vercel.app/dasboard"
+      ? "https://cloud.ashutoshdev.me/dasboard"
       : "http://localhost:3000/dashboard"
   );
 }
